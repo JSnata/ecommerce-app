9311e51b2e658da3c482211faab4b338e6581282
natagetup <n.kulikouskaya@gmail.com>
2024-05-02 19:35:13 +0200

    chore: add husky

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,7 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      Ecommerce app
-    </div>
-  );
+  return <div className="App">Ecommerce app</div>;
 }
 
 export default App;
