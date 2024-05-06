017d7ac04e8eead3f076817e608a4d540e5e485e
LinderJK <slepchenkovia@gmail.com>
2024-05-06 20:11:12 +0300

    fix: add homePage in app, add bootstrap styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
-
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import HomePage from './pages/HomePage';
 
 function App() {
-  return <div className="App">Ecommerce app</div>;
+  return <HomePage />;
 }
 
 export default App;
