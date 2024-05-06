efbc2f8099bd3e0f494fc908381d9a0ff7d8cea7
SPHsofi <109211098+SPHsofi@users.noreply.github.com>
2024-05-06 20:58:39 +0300

    feat: add wrapper for form, add class for form and add title for login page

--- src/ui/LoginForm/LoginForm.tsx
+++ src/ui/LoginForm/LoginForm.tsx
@@ -1,20 +1,24 @@
 import React from 'react';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
+import styles from './LoginForm.module.css';
 
 function LoginForm() {
   return (
-    <Form>
-      <Form.Group className="email">
-        <Form.Label>Email address</Form.Label>
-        <Form.Control type="email" placeholder="name@example.com" />
-      </Form.Group>
-      <Form.Group className="password">
-        <Form.Label>Password</Form.Label>
-        <Form.Control type="password" />
-      </Form.Group>
-      <Button variant="dark">Dark</Button>
-    </Form>
+    <div className={styles.formWrapper}>
+      <h1>Sign up</h1>
+      <Form className={styles.form}>
+        <Form.Group className="email">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control type="email" placeholder="name@example.com" />
+        </Form.Group>
+        <Form.Group className="password">
+          <Form.Label>Password</Form.Label>
+          <Form.Control type="password" />
+        </Form.Group>
+        <Button variant="dark">Dark</Button>
+      </Form>
+    </div>
   );
 }
 
