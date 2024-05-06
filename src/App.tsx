7815bc6cb6f19ea5da7e469a78db77ce89cef039
SPHsofi <109211098+SPHsofi@users.noreply.github.com>
2024-05-06 18:41:50 +0300

    feat: import login form component and use them

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import LoginForm from './ui/LoginForm';
 
 import './App.css';
 
 function App() {
-  return <div className="App">Ecommerce app</div>;
+  return (
+    <>
+      <div className="App">Ecommerce app</div>
+      <LoginForm />
+    </>
+  );
 }
 
 export default App;
