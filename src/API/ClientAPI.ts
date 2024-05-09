f1b18a6112d00844847f972f68bd46e76b18524c
LinderJK <slepchenkovia@gmail.com>
2024-05-09 16:02:04 +0300

    fix: move client api add create customer

--- src/API/ClientAPI.ts
+++ src/API/ClientAPI.ts
@@ -1,20 +1,66 @@
+import { CustomerDraft } from '@commercetools/platform-sdk';
 import { getApiRoot } from './root/BuildClient';
+import { ICustomerCreateData } from '../types/CustomerTypes';
 
 /**
  * Retrieves the API root for the authenticated client.
  * Needs to manage the project and all data
  */
-const apiRoot = getApiRoot();
+export const apiRoot = getApiRoot();
+
+/**
+ * Retrieves the Current Store ID
+ */
+const storeId = '59ad2c93-402f-4af9-9728-d9a2c65f6e86';
 
 /**
  * Retrieves store data from the API.
  * @returns {Promise<any>} A Promise that resolves with the store data if successful, or null if there was an error.
  */
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-const getStoreData = async () => {
+export const getStoreData = async () => {
   try {
     const response = await apiRoot.stores().get().execute();
     return response.body;
+  } catch (error) {
+    console.error('Error creating customer:', error);
+    return null;
+  }
+};
+
+/**
+ * Gets the next available customer number.
+ * @returns {Promise<string>} A Promise that resolves with the next available customer number.
+ */
+const getCustomerNumber = async () => {
+  try {
+    const response = await apiRoot.customers().get().execute();
+    return (response.body.count + 1).toString();
+  } catch (error) {
+    console.error('Error get customer number:', error);
+    return '';
+  }
+};
+
+/**
+ * Creates a new customer with the provided data.
+ * @param {ICustomerCreateData} data - The data for creating the customer.
+ * @returns {Promise<CustomerDraft | null>} A Promise that resolves with the created customer or null if there was an error.
+ */
+export const createCustomer = async (data: ICustomerCreateData): Promise<CustomerDraft | null> => {
+  try {
+    const customerNumber = await getCustomerNumber();
+    const response = await apiRoot
+      .customers()
+      .post({
+        body: {
+          stores: [{ typeId: 'store', id: storeId }],
+          ...data,
+          customerNumber,
+        },
+      })
+      .execute();
+    return response.body.customer;
   } catch (error) {
     console.error('Error creating customer:', error);
     return null;
--- src/API/CustomerAPI.ts
+++ src/API/CustomerAPI.ts
@@ -1,15 +1,9 @@
-import { CustomerDraft } from '@commercetools/platform-sdk';
 import { getApiUser } from './root/BuildUser';
-import { ICustomerCreateData } from '../types/CustomerTypes';
 
 /**
  * Retrieves the API root for the authenticated user..
  */
-export const apiUser = getApiUser();
-/**
- * Retrieves the Current Store ID
- */
-const storeId = '59ad2c93-402f-4af9-9728-d9a2c65f6e86';
+const apiUser = getApiUser();
 
 /**
  * Signs in a customer with the provided email and password.
@@ -18,6 +12,7 @@ const storeId = '59ad2c93-402f-4af9-9728-d9a2c65f6e86';
  * @returns {Promise<void>} A Promise that resolves when the customer is signed in successfully.
  */
 export const signingCustomer = async (email: string, password: string) => {
+  // apiUser = getApiUser(password, email);
   try {
     const response = await apiUser
       .me()
@@ -28,48 +23,11 @@ export const signingCustomer = async (email: string, password: string) => {
           password,
         },
       })
-      .clientRequest();
+      .execute();
     console.log(response);
   } catch (error) {
     console.error(error);
   }
 };
 
-/**
- * Gets the next available customer number.
- * @returns {Promise<string>} A Promise that resolves with the next available customer number.
- */
-const getCustomerNumber = async () => {
-  try {
-    const response = await apiUser.customers().get().execute();
-    return (response.body.count + 1).toString();
-  } catch (error) {
-    console.error('Error get customer number:', error);
-    return '';
-  }
-};
-
-/**
- * Creates a new customer with the provided data.
- * @param {ICustomerCreateData} data - The data for creating the customer.
- * @returns {Promise<CustomerDraft | null>} A Promise that resolves with the created customer or null if there was an error.
- */
-export const createCustomer = async (data: ICustomerCreateData): Promise<CustomerDraft | null> => {
-  try {
-    const customerNumber = await getCustomerNumber();
-    const response = await apiUser
-      .customers()
-      .post({
-        body: {
-          stores: [{ typeId: 'store', id: storeId }],
-          ...data,
-          customerNumber,
-        },
-      })
-      .execute();
-    return response.body.customer;
-  } catch (error) {
-    console.error('Error creating customer:', error);
-    return null;
-  }
-};
+export default signingCustomer;
