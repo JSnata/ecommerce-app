7b1f1861d2c343ee7b3f71539d4f8c758c86cc45
SPHsofi <109211098+SPHsofi@users.noreply.github.com>
2024-05-09 13:18:05 +0300

    feat: add show and hide password

--- src/pages/Auth/LoginPage/LoginPage.tsx
+++ src/pages/Auth/LoginPage/LoginPage.tsx
@@ -1,11 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Form from 'react-bootstrap/Form';
+import InputGroup from 'react-bootstrap/InputGroup';
+import { Eye, EyeSlashFill } from 'react-bootstrap-icons';
 import Button from 'react-bootstrap/Button';
 import { Formik } from 'formik';
 import * as yup from 'yup';
 import styles from './LoginPage.module.css';
 
 function LoginForm() {
+  const [showPass, setShowPass] = useState(false);
+  const clickHandler = () => {
+    setShowPass((prev) => !prev);
+  };
+
   const validationSchema = yup.object().shape({
     email: yup
       .string()
@@ -54,13 +61,16 @@ function LoginForm() {
             </Form.Group>
             <Form.Group className="password">
               <Form.Label>Password</Form.Label>
-              <Form.Control
-                type="password"
-                name="password"
-                value={values.password}
-                onChange={handleChange}
-                isInvalid={touched.password && !!errors.password}
-              />
+              <InputGroup>
+                <Form.Control
+                  type={showPass ? 'text' : 'password'}
+                  name="password"
+                  value={values.password}
+                  onChange={handleChange}
+                  isInvalid={touched.password && !!errors.password}
+                />
+                <InputGroup.Text onClick={clickHandler}>{showPass ? <Eye /> : <EyeSlashFill />}</InputGroup.Text>
+              </InputGroup>
               <Form.Control.Feedback type="invalid">{errors.password}</Form.Control.Feedback>
             </Form.Group>
             <Button variant="dark" type="submit" disabled={!isValid}>
