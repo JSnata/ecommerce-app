a8b7dce35353c05f464dd5014bc0d4e1eefc1817
LinderJK <slepchenkovia@gmail.com>
2024-05-09 15:03:28 +0300

    docs: add JSDoc

--- src/API/ClientAPI.ts
+++ src/API/ClientAPI.ts
@@ -1,6 +1,16 @@
 import { getApiRoot } from './root/BuildClient';
 
+/**
+ * Retrieves the API root for the authenticated client.
+ * Needs to manage the project and all data
+ */
 const apiRoot = getApiRoot();
+
+/**
+ * Retrieves store data from the API.
+ * @returns {Promise<any>} A Promise that resolves with the store data if successful, or null if there was an error.
+ */
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 const getStoreData = async () => {
   try {
     const response = await apiRoot.stores().get().execute();
@@ -10,5 +20,3 @@ const getStoreData = async () => {
     return null;
   }
 };
-
-export default getStoreData;
