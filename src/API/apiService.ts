b618b503a74018a40eb641f3595eef528269effd
LinderJK <slepchenkovia@gmail.com>
2024-05-09 15:07:17 +0300

    refactor: change env, add TODO

--- src/API/apiService.ts
+++ src/API/apiService.ts
@@ -1,22 +1,22 @@
 import axios from 'axios';
 
 class ApiService {
+  // TODO: needed to switch token to user token
   static headers = {
-    Authorization: `Bearer ${process.env.REACT_APP_API_ACCESS_TOKEN}`,
+    Authorization: `Bearer ${process.env}`,
   };
 
   static createUrl(endpoint: string): string {
-    if (!process.env.REACT_APP_API_URL || !process.env.REACT_APP_API_PROJECT_KEY) {
+    if (!process.env.REACT_APP_CTP_AUTH_URL || !process.env.REACT_APP_CTP_PROJECT_KEY) {
       throw new Error('API_HOST or API_KEY environment variables are not defined');
     }
-    return `${process.env.REACT_APP_API_URL}/${process.env.REACT_APP_API_PROJECT_KEY}/${endpoint}`;
+    return `${process.env.REACT_APP_CTP_AUTH_URL}/${process.env.REACT_APP_CTP_PROJECT_KEY}/${endpoint}`;
   }
 
   static async getData(endpoint: string) {
     const url = ApiService.createUrl(endpoint);
     try {
       const response = await axios.get(`${url}`, { headers: this.headers });
-      // console.log(response);
       return response.data;
     } catch (err) {
       console.error(err);
