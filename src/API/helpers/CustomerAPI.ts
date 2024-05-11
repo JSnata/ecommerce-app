aff57a45db66cbe4f84da4cf20adc921774e6fe4
LinderJK <slepchenkovia@gmail.com>
2024-05-11 17:11:10 +0300

    refactor: change return signingCustomer to new api instance

--- src/API/helpers/CustomerAPI.ts
+++ src/API/helpers/CustomerAPI.ts
@@ -1,21 +1,14 @@
 import { ByProjectKeyRequestBuilder } from '@commercetools/platform-sdk';
-import getApiCustomer from './root/BuildCustomer';
+import { createApiCustomer } from '../root/BuildCustomer';
 
-/**
- * Retrieves the API root for the authenticated Customer..
- */
-let apiCustomer: ByProjectKeyRequestBuilder;
 /**
  * Signs in a customer with the provided email and password.
  * @param {string} username - The email/username of the customer.
  * @param {string} password - The password of the customer.
  * @returns {Promise<void>} A Promise that resolves when the customer is signed in successfully.
  */
-export const signingCustomer = async (username: string, password: string) => {
-  if (!apiCustomer) {
-    console.error('Could not find the API user');
-    apiCustomer = getApiCustomer(username, password);
-  }
+const signingCustomer = async (username: string, password: string): Promise<ByProjectKeyRequestBuilder | undefined> => {
+  const apiCustomer = createApiCustomer(username, password);
   try {
     const response = await apiCustomer
       .me()
@@ -27,16 +20,13 @@ export const signingCustomer = async (username: string, password: string) => {
         },
       })
       .execute();
-    return response;
+    if (response.statusCode === 200) {
+      return apiCustomer;
+    }
   } catch (error) {
     console.error(error);
-    return null;
+    return undefined;
   }
+  return apiCustomer;
 };
-
-export const getCustomerApi = () => {
-  if (apiCustomer) {
-    return apiCustomer;
-  }
-  return null;
-};
+export default signingCustomer;
