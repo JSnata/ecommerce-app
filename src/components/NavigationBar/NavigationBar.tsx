1477a3192ba972fd915f24a1218106db88edebcc
SPHsofi <109211098+SPHsofi@users.noreply.github.com>
2024-05-18 16:47:43 +0300

    feat: add image logo and add new classes for Collapse

--- src/components/NavigationBar/NavigationBar.tsx
+++ src/components/NavigationBar/NavigationBar.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Container, Navbar } from 'react-bootstrap';
 import SecondaryButton from '../../ui/Buttons/SecondaryButton';
+import styles from './NavigationBar.module.css';
 
 function NavigationBar() {
   // console.log(createCustomer({ email: 'HHHHH', password: 'HHHHH', firstName: 'HHHH', lastName: 'HHHH' }));
@@ -8,9 +9,11 @@ function NavigationBar() {
   return (
     <Navbar expand="lg" className="bg-body-tertiary">
       <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Brand href="/">
+          <img src="./logo.png" className={`d-inline-block align-top ${styles.logo}`} alt="React Bootstrap logo" />
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
+        <Navbar.Collapse id="basic-navbar-nav" className="justify-content-center justify-content-lg-end">
           <SecondaryButton to="/login" link="/login">
             Login
           </SecondaryButton>
