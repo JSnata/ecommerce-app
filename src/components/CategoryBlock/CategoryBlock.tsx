3b378eef6ce2c2c2712a55235357a1a37bc5a282
LinderJK <slepchenkovia@gmail.com>
2024-05-19 18:08:39 +0300

    feat: add CategoryBlock component

---
+++ src/components/CategoryBlock/CategoryBlock.tsx
@@ -0,0 +1,87 @@
+import React from 'react';
+import CardItem from '../../ui/Cards/CardItem/CardItem';
+import CardCategory from '../../ui/Cards/CardCategory/CardCategory';
+import styles from './CategoryBlock.module.css';
+
+type CategoryCardProps = {
+  categoryDescription?: string;
+  categoryName: string;
+  buttonLabel: string;
+  onClick: () => void;
+  imageLink: string;
+  reverse?: boolean;
+};
+
+function CategoryBlock(props: CategoryCardProps) {
+  const { reverse, categoryName, buttonLabel, onClick, imageLink, categoryDescription } = props;
+  return (
+    // <Row>
+    //   {reverse ? (
+    //     <>
+    //       <Col>
+    //         <CardItem imageLink={imageLink} />
+    //       </Col>
+    //       <Col xl={8}>
+    //         <CardCategory
+    //           label={categoryName}
+    //           buttonLabel={buttonLabel}
+    //           onClick={onClick}
+    //           description={categoryDescription}
+    //         />
+    //       </Col>
+    //     </>
+    //   ) : (
+    //     <>
+    //       <Col xl={8}>
+    //         <CardCategory
+    //           label={categoryName}
+    //           buttonLabel={buttonLabel}
+    //           onClick={onClick}
+    //           description={categoryDescription}
+    //         />
+    //       </Col>
+    //       <Col>
+    //         <CardItem imageLink={imageLink} />
+    //       </Col>
+    //     </>
+    //   )}
+    // </Row>
+    <div className={styles.categoryBlock}>
+      {reverse ? (
+        <>
+          <div className={styles.itemContainer}>
+            <CardItem imageLink={imageLink} />
+          </div>
+          <div className={styles.cardContainer}>
+            <CardCategory
+              label={categoryName}
+              buttonLabel={buttonLabel}
+              onClick={onClick}
+              description={categoryDescription}
+            />
+          </div>
+        </>
+      ) : (
+        <>
+          <div className={styles.cardContainer}>
+            <CardCategory
+              label={categoryName}
+              buttonLabel={buttonLabel}
+              onClick={onClick}
+              description={categoryDescription}
+            />
+          </div>
+          <div className={styles.itemContainer}>
+            <CardItem imageLink={imageLink} />
+          </div>
+        </>
+      )}
+    </div>
+  );
+}
+
+CategoryBlock.defaultProps = {
+  reverse: false,
+  categoryDescription: '',
+};
+export default CategoryBlock;
