0e83657423c8405f0ecd596d3b24f0fad7b666fe
LinderJK <slepchenkovia@gmail.com>
2024-05-24 21:17:17 +0300

    feat: add link to profile page

--- src/components/NavBarProfile/NavBarProfile.tsx
+++ src/components/NavBarProfile/NavBarProfile.tsx
@@ -1,26 +1,26 @@
 import React from 'react';
 import { Dropdown } from 'react-bootstrap';
 import { toast } from 'react-toastify';
+import { Link } from 'react-router-dom';
 import useAuthContext from '../../hooks/useAuthContext';
 
 function NavBarProfile() {
   const { user, dispatch } = useAuthContext();
-  // const location = useLocation();
-
   const handleLogout = () => {
     dispatch({ type: 'LOGOUT', payload: null });
     localStorage.removeItem('token');
     toast('Logged out');
   };
   return (
-    <Dropdown className="mx-3">
+    <Dropdown className="mx-3" id="user-profile-doropdown">
       <Dropdown.Toggle variant="outline-dark" id="dropdown-basic">
         {user?.firstName}
       </Dropdown.Toggle>
-
       <Dropdown.Menu>
-        <Dropdown.Item>Profile</Dropdown.Item>
-        <Dropdown.Item href="#">Shopping List</Dropdown.Item>
+        <Dropdown.Item as={Link} to="/profile">
+          Profile
+        </Dropdown.Item>
+        <Dropdown.Item href="">Shopping List</Dropdown.Item>
         <Dropdown.Divider />
         <Dropdown.Item onClick={handleLogout}>Sign out</Dropdown.Item>
       </Dropdown.Menu>
