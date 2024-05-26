3d96f2391b9d62a2a9adaeb2c2192cf5f6e3dc6e
SPHsofi <109211098+SPHsofi@users.noreply.github.com>
2024-05-26 18:27:47 +0300

    feat: add components modal window

---
+++ src/components/ModalWindow/ModalWindow.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Modal } from 'react-bootstrap';
+
+interface ModalProps {
+  show: boolean;
+  handleClose: () => void;
+  modalSize: 'sm' | 'lg' | 'xl' | undefined;
+  children: React.ReactNode;
+}
+
+function ModalWindow(props: ModalProps) {
+  const { show, handleClose, modalSize, children } = props;
+  return (
+    <Modal show={show} onHide={handleClose} backdrop="static" keyboard={false} size={modalSize}>
+      <Modal.Header closeButton>
+        <Modal.Title>Modal title</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>{children}</Modal.Body>
+    </Modal>
+  );
+}
+
+export default ModalWindow;
