4a6c2c211e9ad018355bf05980c7471c80957adc
LinderJK <slepchenkovia@gmail.com>
2024-05-27 13:15:12 +0300

    feat: add profilePage with customInputs, add help functions getProfile data and type of field

--- src/pages/UserProfilePage/ProfilePage.tsx
+++ src/pages/UserProfilePage/ProfilePage.tsx
@@ -1,15 +1,69 @@
+import { Formik } from 'formik';
 import React from 'react';
-import { Container } from 'react-bootstrap';
+import { Col, Container, Form, Row } from 'react-bootstrap';
+import { CustomerDraft } from '@commercetools/platform-sdk';
 import useAuthContext from '../../hooks/useAuthContext';
+import profileValidationSchema from '../../utils/validation';
+import CustomTextInput from '../../ui/Input/CustomInput';
+
+type CustomerSubset = Pick<
+  CustomerDraft,
+  | 'dateOfBirth'
+  | 'defaultBillingAddress'
+  | 'defaultShippingAddress'
+  | 'email'
+  | 'firstName'
+  | 'lastName'
+  | 'password'
+  | 'shippingAddresses'
+  | 'billingAddresses'
+>;
+
+const getInputType = (key: string): string => {
+  switch (key) {
+    case 'dateOfBirth':
+      return 'date';
+    case 'password':
+      return 'password';
+    default:
+      return 'text';
+  }
+};
+
+function getCustomerProfileData(user: CustomerDraft): CustomerSubset {
+  return {
+    email: user.email,
+    firstName: user.firstName,
+    lastName: user.lastName,
+    password: '',
+    dateOfBirth: user.dateOfBirth,
+  };
+}
 
 function ProfilePage() {
   const { user } = useAuthContext();
-  console.log('user', user);
+  const userData = getCustomerProfileData(user!);
   return (
-    <Container className="my-5">
-      <p>Profile</p>
-      <p>{user?.firstName}</p>
-      <p>{user?.email}</p>
+    <Container>
+      <Formik initialValues={userData} validationSchema={profileValidationSchema} onSubmit={() => {}}>
+        {() => (
+          <Form>
+            {Object.entries(userData).map(([key]) => (
+              <Row key={key}>
+                <Col md={6} className="my-2">
+                  <CustomTextInput
+                    label={key.charAt(0).toUpperCase() + key.slice(1)}
+                    name={key}
+                    type={getInputType(key)}
+                    placeholder=""
+                    isEditable
+                  />
+                </Col>
+              </Row>
+            ))}
+          </Form>
+        )}
+      </Formik>
     </Container>
   );
 }
