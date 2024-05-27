8848cbf312ec124cf1fb0f08c3064896b27dc2ab
SPHsofi <109211098+SPHsofi@users.noreply.github.com>
2024-05-27 09:59:48 +0300

    refactor: change column size in small device

--- src/pages/ProductPage/ProductPage.tsx
+++ src/pages/ProductPage/ProductPage.tsx
@@ -30,10 +30,10 @@ function ProductPage() {
   return (
     <div>
       <Row>
-        <Col sm={10} md={6} className={`${style.col} ${style.left}`} onClick={openModal}>
+        <Col sm={12} md={6} className={`${style.col} ${style.left}`} onClick={openModal}>
           <CarouselComponent srcArray={productSrcArray} interval={null} id={productId} />
         </Col>
-        <Col sm={10} md={6} className={`${style.col} ${style.right}`}>
+        <Col sm={12} md={6} className={`${style.col} ${style.right}`}>
           <Row>
             <Col>
               <h2 className={style.name}>{productName}</h2>
