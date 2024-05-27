d32f41919334b8ab5f813838a683491db143230b
LinderJK <slepchenkovia@gmail.com>
2024-05-27 18:57:53 +0300

    refactor: fix styles

--- src/pages/UserProfilePage/ProfilePage.tsx
+++ src/pages/UserProfilePage/ProfilePage.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Col, Container, Row } from 'react-bootstrap';
 import useAuthContext from '../../hooks/useAuthContext';
 import profileValidationSchema from '../../utils/validation';
 import { getCustomerMainProfileData } from './userProfileUtils';
@@ -11,7 +12,16 @@ function ProfilePage() {
   // const addressesData = getCustomerAddressData(user!);
   console.log('user data', userData);
   // console.log('adresses data', addressesData);
-  return <ProfileMainData data={userData} validationSchema={profileValidationSchema} />;
+  return (
+    <Container>
+      <Row>
+        <Col>
+          <ProfileMainData data={userData} validationSchema={profileValidationSchema} />
+        </Col>
+        <Col>123</Col>
+      </Row>
+    </Container>
+  );
 }
 
 export default ProfilePage;
