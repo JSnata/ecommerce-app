f06fbc338834f646da75f1ead817deb083bca186
SPHsofi <109211098+SPHsofi@users.noreply.github.com>
2024-05-28 10:18:21 +0300

    refactor: choose every images and add array with url for each, add deferent price

--- src/pages/ProductPage/ProductPage.tsx
+++ src/pages/ProductPage/ProductPage.tsx
@@ -9,13 +9,15 @@ function ProductPage() {
   const products = useProducts();
   const product = products[0];
   const productId = product?.id;
-  const productSrc1 = product?.masterData?.current?.masterVariant?.images?.[0]?.url;
-  const productSrc2 = product?.masterData?.current?.masterVariant?.images?.[1]?.url;
-  const productSrc3 = product?.masterData?.current?.masterVariant?.images?.[2]?.url;
+  const images = product?.masterData?.current?.masterVariant?.images;
+  const productSrcArray: (string | undefined)[] = [];
+  images?.forEach((img) => {
+    productSrcArray.push(img?.url);
+  });
   const productName = product?.masterData?.current?.name?.['en-GB'];
   const productDescription = product?.masterData?.current?.description?.['en-GB'];
-  const productPrice = product?.masterData?.current?.masterVariant?.prices?.[1]?.value?.centAmount;
-  const productSrcArray = [productSrc1, productSrc2, productSrc3];
+  const productPriceCurr = product?.masterData?.current?.masterVariant?.prices?.[1]?.value?.centAmount;
+  const productPriceStaged = product?.masterData?.staged?.masterVariant?.prices?.[1]?.value?.centAmount;
   const [showModal, setShowModal] = useState(false);
 
   const openModal = (event: React.MouseEvent<HTMLElement>) => {
@@ -26,7 +28,7 @@ function ProductPage() {
   };
   const handleClose = () => setShowModal(false);
 
-  console.log(products[0]);
+  console.log(productSrcArray);
   return (
     <div>
       <Row>
@@ -46,7 +48,12 @@ function ProductPage() {
           </Row>
           <Row>
             <Col>
-              <h2>{productPrice?.toLocaleString()}$</h2>
+              <h2>{productPriceCurr?.toLocaleString()}$</h2>
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+              <h3 className={style.oldPrice}>{productPriceStaged?.toLocaleString()}$</h3>
             </Col>
           </Row>
         </Col>
