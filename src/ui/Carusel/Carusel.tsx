5e169ffe0759db391128e128695128ec25e9f9dc
SPHsofi <109211098+SPHsofi@users.noreply.github.com>
2024-06-04 16:23:54 +0300

    feat: add carousel class into props and use it into property carousel

--- src/ui/Carusel/Carusel.tsx
+++ src/ui/Carusel/Carusel.tsx
@@ -6,13 +6,20 @@ interface CarouselData {
   interval: null | number;
   id: undefined | string;
   imgClassName?: string;
+  carouselClass?: string;
 }
 
 function CarouselComponent(props: CarouselData) {
-  const { srcArray, interval, id, imgClassName } = props;
+  const { srcArray, interval, id, imgClassName, carouselClass } = props;
   const getKeyByIndex = (index: number) => `${id}-${index}`;
   return (
-    <Carousel interval={interval} data-bs-theme="dark" controls={srcArray.length > 1} indicators={srcArray.length > 1}>
+    <Carousel
+      className={carouselClass}
+      interval={interval}
+      data-bs-theme="dark"
+      controls={srcArray.length > 1}
+      indicators={srcArray.length > 1}
+    >
       {srcArray.map((src, index) => (
         <Carousel.Item key={getKeyByIndex(index)}>
           <Image src={src} className={imgClassName} alt="Product photo" />
