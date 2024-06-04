1c964898d25547b9f722485276121d0924b371e3
JSNata <n.kulikouskaya@gmail.com>
2024-06-05 00:03:14 +0200

    feat: implemented search functionality

--- src/hooks/useProductsByCategory.ts
+++ src/hooks/useProductsByCategory.ts
@@ -7,21 +7,44 @@ type FilterOptions = {
   colorFlower?: string;
   sizeFlower?: string;
   sort?: string | null;
+  search?: string | null;
+  language?: string;
 };
 
-const useProductsByCategory = ({ categoryId, colorFlower, sizeFlower, sort }: FilterOptions) => {
+const useProductsByCategory = ({
+  categoryId,
+  colorFlower,
+  sizeFlower,
+  sort,
+  search,
+  language = 'en-GB',
+}: FilterOptions) => {
   const [products, setProducts] = useState<ProductProjection[]>([]);
   const [loading, setLoading] = useState<boolean>(true);
   const [fetchError, setFetchError] = useState<string | null>(null);
 
-  const fetchProducts = async (filters: string[], sortOption: string | null | undefined) => {
+  const fetchProducts = async (
+    filters: string[],
+    sortOption: string | null | undefined,
+    searchQuery: string | null | undefined,
+    languageQuery: string,
+  ) => {
     setLoading(true);
     setFetchError(null);
     try {
-      const queryArgs: { filter: string[]; sort?: string } = { filter: filters };
+      const queryArgs: { filter: string[]; sort?: string; [key: string]: string | string[] | undefined | boolean } = {
+        filter: filters,
+      };
+
       if (sortOption) {
         queryArgs.sort = sortOption;
       }
+
+      if (searchQuery) {
+        queryArgs[`text.${languageQuery}`] = searchQuery;
+        queryArgs.fuzzy = true;
+      }
+
       const response = await apiRoot.productProjections().search().get({ queryArgs }).execute();
       setProducts(response.body.results);
     } catch (err) {
@@ -38,8 +61,8 @@ const useProductsByCategory = ({ categoryId, colorFlower, sizeFlower, sort }: Fi
     if (colorFlower) filters.push(`variants.attributes.color-flower.key:"${colorFlower}"`);
     if (sizeFlower) filters.push(`variants.attributes.size-flower.key:"${sizeFlower}"`);
 
-    fetchProducts(filters, sort);
-  }, [categoryId, colorFlower, sizeFlower, sort]);
+    fetchProducts(filters, sort, search, language);
+  }, [categoryId, colorFlower, sizeFlower, sort, search, language]);
 
   return { products, loading, error: fetchError };
 };
