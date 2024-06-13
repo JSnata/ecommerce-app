e3f2f6955f12191cd4b7e07b9b65c226e67363d2
SPHsofi <109211098+SPHsofi@users.noreply.github.com>
2024-06-13 16:18:37 +0300

    refactor: add bio for people and change class

--- src/pages/About/AboutPage.tsx
+++ src/pages/About/AboutPage.tsx
@@ -12,15 +12,20 @@ export default function AboutPage() {
     <Row className={style.row}>
       <Col>
         <Row className={`border border-1 border-dark ${style.row}`}>
-          <Col>
+          <Col className="d-flex flex-column text-center p-5">
             <Row className={style.row}>
-              <Col className="d-flex justify-content-center">
+              <Col>
                 <h1>Our story</h1>
               </Col>
             </Row>
-            <Row className={style.row}>
-              <Col className="d-flex justify-content-center">
-                <p>Text text text text text text text about our team</p>
+            <Row className={`justify-content-center ${style.row}`}>
+              <Col sm={10}>
+                <p>
+                  The team was assembled from participants whom the mentor chose as his wards. Since February, we began
+                  to communicate a lot and help each other with individual tasks. When it came time to assemble teams
+                  for a group project, we did not hesitate to form a team. Our communication is set up well, Discord has
+                  sections for specific occasions, and Trello helps us manage tasks.
+                </p>
               </Col>
             </Row>
           </Col>
@@ -33,7 +38,8 @@ export default function AboutPage() {
             style={style.infoNata}
             fio="Natallia Kulikouskaya"
             post="Team Lead / Frontend developer"
-            bio="Text text text text text Text text text text text Text text text text text Text text text text text"
+            bio="I’m from the Republic of Belarus, but now I live in Poland, while it’s difficult to get a normal job, before moving I worked as an SEO, now I’m learning English and taking this course to gain courage and finally go look for an offer. I decided to temporarily put my hobby aside and kick myself to study and do more business, although in quiet times I did drawing, I love books and walks, mountains and much more)"
+            did="Api connect, router, catalog page, header, "
             gitHub="https://github.com/jsnata"
           />
         </Row>
@@ -42,7 +48,8 @@ export default function AboutPage() {
             style={style.infoIlya}
             fio="Ilya Slepchenkov"
             post="Frontend developer"
-            bio="Text text text text text Text text text text text Text text text text text Text text text text text"
+            bio="I work in Moscow (Zelenograd) as an engineer in the field of microelectronics and study at rsschool, but there is no time for the rest. Hobbies like most: books, TV series, some games, longboard."
+            did="Api connect, main page, basket page"
             gitHub="https://github.com/linderjk"
           />
           <Col xs={12} md={6} className={style.imgIlya}>
@@ -57,7 +64,8 @@ export default function AboutPage() {
             style={style.infoSofa}
             fio="Sofia Sharshunskaya"
             post="Frontend developer"
-            bio="Text text text text text Text text text text text Text text text text text Text text text text text"
+            bio="I work in Moscow. Since high school, I wanted to work in the IT field, but I could not decide on the direction. I went to the institute in the direction of 'applied informatics'. I studied for 4 years and got a diploma. I started working as a database designer and developer, but this is not what I would like to do all my life. I realized that I liked JavaScript and began to delve into its study, got into RS School and now I'm trying to live to the end of the stage."
+            did="Login/Register page, product page, page about, validation logig"
             gitHub="https://github.com/SPHsofi"
           />
         </Row>
