3427487f5282e988af7b096a5b9269bfc3b1569e
Linderjk <linder25@yandex.ru>
2024-06-13 14:38:32 +0300

    feat: add order summary component

---
+++ src/pages/OrderPage/OrderSummary.tsx
@@ -0,0 +1,13 @@
+// type OrderSummaryProps = {
+//   image: string;
+//   name: string;
+//   quantity: string;
+//   totalPrice: string;
+//   handleAdd: () => void;
+// };
+
+// function OrderSummary() {
+//   return <div>123</div>;
+// }
+
+// export default OrderSummary;
