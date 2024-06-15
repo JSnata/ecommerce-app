0fe8dc5f1273544d38ae2e18bf87ed4f1c5fdf7b
LinderJK <slepchenkovia@gmail.com>
2024-06-15 10:45:46 +0300

    refactor: change main page promo card styles

--- src/pages/OrderPage/OrderPromoView.tsx
+++ src/pages/OrderPage/OrderPromoView.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Card, CardGroup, Container } from 'react-bootstrap';
+import { Card, CardGroup, Col, Container, Row } from 'react-bootstrap';
 import type { DiscountCode } from '@commercetools/platform-sdk';
 import usePromo from '../../hooks/usePromo';
 
@@ -10,28 +10,34 @@ function OrderPromo() {
 
   return (
     <Container>
-      {promo && promo.length > 0 ? (
-        <CardGroup>
-          <h5>
-            <small className="text-muted">Available promocodes</small>
-          </h5>
-          <small className="text-muted">Enter in Cart discount field</small>
-          {promo.map((item: DiscountCode) => {
-            const description = item?.description?.['en-GB'];
-            const name = item.code;
-            return (
-              <Card key={item.id}>
-                <Card.Body>
-                  <Card.Title>{name}</Card.Title>
-                  <Card.Text>{description}</Card.Text>
-                </Card.Body>
-              </Card>
-            );
-          })}
-        </CardGroup>
-      ) : (
-        ''
-      )}
+      <Row>
+        <h4>Available promo codes</h4>
+      </Row>
+      <Row>
+        <small className="text-muted">Enter in Cart discount field</small>
+      </Row>
+      <Row>
+        {promo && promo.length > 0 ? (
+          <CardGroup>
+            {promo.map((item: DiscountCode) => {
+              const description = item?.description?.['en-GB'];
+              const name = item.code;
+              return (
+                <Col>
+                  <Card key={item.id}>
+                    <Card.Body>
+                      <Card.Title>{name}</Card.Title>
+                      <Card.Text>{description}</Card.Text>
+                    </Card.Body>
+                  </Card>
+                </Col>
+              );
+            })}
+          </CardGroup>
+        ) : (
+          ''
+        )}
+      </Row>
     </Container>
   );
 }
