80db9937630f6aa5237704f524ec8673f4d87780
LinderJK <slepchenkovia@gmail.com>
2024-06-15 17:27:36 +0300

    feat: add conditional render for input then promo is active

--- src/pages/OrderPage/OrderPromoInput.tsx
+++ src/pages/OrderPage/OrderPromoInput.tsx
@@ -1,64 +1,85 @@
 import { Formik, FormikHelpers, FormikProps } from 'formik';
 import * as yup from 'yup';
-import React, { useCallback } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import { Button, Col, Container, Form, Row } from 'react-bootstrap';
+import { DiscountCode } from '@commercetools/platform-sdk';
 import usePromo from '../../hooks/usePromo';
 
 interface PromoFormValues {
-  promo: string;
+  promoName: string;
 }
 
 function OrderPromoInput() {
-  const { addPromo } = usePromo();
+  const { addPromo, removePromo, appliedPromoCode, promos } = usePromo();
+  const [currentPromo, setCurrentPromo] = useState<DiscountCode | undefined>(undefined);
   const schema = yup.object().shape({
-    promo: yup.string().min(2, 'Too Short').max(10, 'Too long').required('Cant be empty'),
+    promoName: yup.string().min(2, 'Too Short').max(10, 'Too long').required('Cannot be empty'),
+  });
+
+  useEffect(() => {
+    const current = promos?.find((promo) => promo.id === appliedPromoCode?.discountCode.id);
+    if (current) {
+      setCurrentPromo(current);
+    }
   });
 
   const handleSubmitPromo = useCallback(
     (values: PromoFormValues, { setSubmitting }: FormikHelpers<PromoFormValues>) => {
-      console.log('submit', values);
-      addPromo(values.promo).then((response) => {
-        console.log('response promo', response);
+      addPromo(values.promoName).then(() => {
         setSubmitting(false);
       });
     },
-    [],
+    [addPromo],
   );
+
+  const handleDeletePromo = useCallback(() => {
+    removePromo();
+  }, [removePromo]);
 
   return (
     <Container className="my-5">
       <h5>Use promo code</h5>
-      <Formik
-        initialValues={{
-          promo: '',
-        }}
-        validationSchema={schema}
-        onSubmit={handleSubmitPromo}
-      >
-        {({ handleSubmit, handleChange, values, touched, errors }: FormikProps<PromoFormValues>) => (
-          <Form noValidate onSubmit={handleSubmit}>
-            <Form.Group as={Row} controlId="">
-              <Col sm="8">
-                <Form.Control
-                  type="text"
-                  name="promo"
-                  value={values.promo}
-                  onChange={handleChange}
-                  isValid={touched.promo && !errors.promo}
-                  isInvalid={!!errors.promo}
-                />
-                <Form.Control.Feedback type="invalid">{errors.promo}</Form.Control.Feedback>
-                <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
-              </Col>
-              <Col sm="2">
-                <Button variant="dark" type="submit">
-                  Add
-                </Button>
-              </Col>
-            </Form.Group>
-          </Form>
-        )}
-      </Formik>
+      {appliedPromoCode ? (
+        <div>
+          <Form.Control type="text" placeholder={currentPromo?.code || '123'} disabled readOnly />
+          <br />
+          <Button variant="dark" type="button" onClick={handleDeletePromo}>
+            Remove
+          </Button>
+        </div>
+      ) : (
+        <Formik
+          initialValues={{
+            promoName: '',
+          }}
+          validationSchema={schema}
+          onSubmit={handleSubmitPromo}
+        >
+          {({ handleSubmit, handleChange, values, touched, errors }: FormikProps<PromoFormValues>) => (
+            <Form noValidate onSubmit={handleSubmit}>
+              <Form.Group as={Row} controlId="">
+                <Col sm="8">
+                  <Form.Control
+                    type="text"
+                    name="promoName"
+                    value={values.promoName}
+                    onChange={handleChange}
+                    isValid={touched.promoName && !errors.promoName}
+                    isInvalid={!!errors.promoName}
+                  />
+                  <Form.Control.Feedback type="invalid">{errors.promoName}</Form.Control.Feedback>
+                  <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+                </Col>
+                <Col sm="2">
+                  <Button variant="dark" type="submit">
+                    Add
+                  </Button>
+                </Col>
+              </Form.Group>
+            </Form>
+          )}
+        </Formik>
+      )}
     </Container>
   );
 }
